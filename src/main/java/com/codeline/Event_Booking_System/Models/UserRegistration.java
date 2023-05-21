11118922727eaed854a002a4737a94271e4d7880
awadh3030 <awadh3030@hotmail.com>
2023-05-21 12:08:15 +0400

    add userRegisRO

--- src/main/java/com/codeline/Event_Booking_System/Models/UserRegistration.java
+++ src/main/java/com/codeline/Event_Booking_System/Models/UserRegistration.java
@@ -1,6 +1,18 @@
 package com.codeline.Event_Booking_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+@Data
 public class UserRegistration {
+    @Id
 
     Long id;
     String  username;
